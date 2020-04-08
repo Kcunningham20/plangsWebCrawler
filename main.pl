a63493b142a2ac81d8ed6178cdeb583ab34aaa0a
Kcunningham20 <31940556+Kcunningham20@users.noreply.github.com>
2020-04-08 16:07:26 -0700

    Started foundation of the crawler

--- main.pl
+++ main.pl
@@ -1,1 +1,26 @@
-Yo
+require HTTP::Request;
+require LWP::UserAgent;
+
+open(INFILE, "target.txt");
+#@lines = <INFILE>;
+#print @lines;
+$line = <INFILE>; #read in first line
+close(INFILE);
+
+print "\nCurrent Line Crawling: " .$line ."\n";
+
+$req = HTTP::Request->new(GET=> $line);
+$ua = LWP::UserAgent->new;
+$response = $ua->request($req);
+#print $response->content;
+@resp = split(/\n/, $response->content);
+foreach $r (@resp) {
+	chomp $r;
+	print $r."\n";
+}
+
+#open(INFILE, "url.txt");
+#add current site to this
+#open(INFILE, "target.txt");
+#add all urls we found on the current page
+#close(INFILE);
